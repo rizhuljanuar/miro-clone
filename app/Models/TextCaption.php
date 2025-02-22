017879bf77f324ad398e01fd2b25fb0447ed9642
rizhul.januar <rizhul.januar@kopnus.com>
2025-02-22 16:52:55 +0700

    feat: setup model, migration and auth passport, socialite

---
+++ app/Models/TextCaption.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+class TextCaption extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, HasApiTokens, Notifiable;
+
+    public static function createUser($googleUser)
+    {
+        $password = Str::random(10);
+        $user = User::updateOrCreate([
+            'googleId' => $googleUser->id,
+        ], [
+            'name' => $googleUser->name,
+            'email' => $googleUser->email,
+            'password' => Hash::make($password),
+        ]);
+
+        return $user;
+    }
 
     /**
      * The attributes that are mass assignable.
